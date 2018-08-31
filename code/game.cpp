35ec90fbecdcd1427fa3c9a026bb94deb5694782
aod6060 <a.o.d.6060@gmail.com>
2018-08-31 01:04:12 -0500

    Final Fixed the mistake I made with the the git reset command

--- code/game.cpp
+++ code/game.cpp
@@ -1,17 +1,71 @@
 #include "game.h"
 
+// Shader
+static Shader vertex;
+static Shader frag;
+// Program
+static Program prog;
+// Vertex Buffer
+static VertexBuffer vBuf;
+// Color Buffer
+static VertexBuffer cBuf;
+
+static float rot = 0.0f;
 
 void game_init()
 {
+	glEnable(GL_DEPTH_TEST);
+
+	// Shaders
+	vertex.create(GL_VERTEX_SHADER, "data/shaders/main.vert");
+	frag.create(GL_FRAGMENT_SHADER, "data/shaders/main.frag");
+
+	// Program
+	prog.addShader(&vertex);
+	prog.addShader(&frag);
+	prog.create();
+	prog.bind();
+
+	// Attributes
+	prog.getAttr()->set("vertices", 0);
+	prog.getAttr()->set("vertexColors", 1);
+	prog.getAttr()->bind();
+	prog.getAttr()->enable("vertices");
+	prog.getAttr()->enable("vertexColors");
+	prog.getAttr()->unbind();
+	prog.getAttr()->disable("vertices");
+	prog.getAttr()->disable("vertexColors");
+
+	// Uniforms
+	prog.getUniforms()->create("proj");
+	prog.getUniforms()->create("view");
+	prog.getUniforms()->create("model");
+
+	prog.unbind();
+
+	// Vertex Buffer
+	vBuf.add(0.0f, 1.0f, 0.0f);
+	vBuf.add(1.0f, -1.0f, 0.0f);
+	vBuf.add(-1.0f, -1.0f, 0.0f);
+	vBuf.create();
+	vBuf.upload();
+
+	// Color Buffer
+	cBuf.add(1.0f, 0.0f, 0.0f, 1.0f);
+	cBuf.add(0.0f, 1.0f, 0.0f, 1.0f);
+	cBuf.add(0.0f, 0.0f, 1.0f, 1.0f);
+	cBuf.create();
+	cBuf.upload();
+
 
 }
 
 void game_update(float delta)
 {
-
-	if (input_isKeyPress(SDL_SCANCODE_LEFT))
+	rot += 64.0f * delta;
+	if (rot > 0)
 	{
-		std::cout << "Hello, World Left [PRESS]" << std::endl;
+		rot -= 360.0f;
 	}
 }
 
@@ -20,13 +74,56 @@ void game_fixedUpdate()
 
 }
 
+#define TO_FLOAT_C(r) (r / 255.0f)
+
 void game_render()
 {
+	glm::mat4 proj =
+		glm::perspective(
+			glm::radians(45.0f), 
+			(float)app_getWidth() / (float)app_getHeight(), 
+			1.0f, 
+			1024.0f);
 
+	glm::mat4 view =
+		glm::mat4(1.0f);
+
+	glm::mat4 model =
+		glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f)) *
+		glm::rotate(glm::mat4(1.0f), glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	glClearColor(TO_FLOAT_C(0), TO_FLOAT_C(191), TO_FLOAT_C(255), 1.0F);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	prog.bind();
+
+	prog.getUniforms()->setMat4("proj", proj);
+	prog.getUniforms()->setMat4("view", view);
+	prog.getUniforms()->setMat4("model", model);
+
+	prog.getAttr()->bind();
+
+	vBuf.bind();
+	prog.getAttr()->pointer("vertices", 3, GL_FLOAT);
+	vBuf.unbind();
+
+	cBuf.bind();
+	prog.getAttr()->pointer("vertexColors", 4, GL_FLOAT);
+	cBuf.unbind();
+
+	glDrawArrays(GL_TRIANGLES, 0, vBuf.size() / 3);
+
+	prog.getAttr()->unbind();
+
+	prog.unbind();
 }
 
 void game_release()
 {
-
+	cBuf.release();
+	vBuf.release();
+	prog.release();
+	frag.release();
+	vertex.release();
 }
 
--- code/sys.h
+++ code/sys.h
@@ -76,3 +76,121 @@ void input_toggleGrab();
 
 
 // Graphcis
+
+// Vertex Buffer
+class VertexBuffer
+{
+private:
+	uint32_t id;
+	std::vector<float> list;
+public:
+	void add(float x);
+	void add(float x, float y);
+	void add(const glm::vec2& v);
+	void add(float x, float y, float z);
+	void add(const glm::vec3& v);
+	void add(float x, float y, float z, float w);
+	void add(const glm::vec4& v);
+	void clear();
+	void create();
+	void upload();
+	void bind();
+	void unbind();
+	void release();
+	uint32_t size();
+};
+
+// Index Buffer
+// Attributes
+class Attributes
+{
+private:
+	uint32_t id;
+	std::map <std::string, uint32_t> attributes;
+public:
+	void set(std::string name, uint32_t id);
+	void enable(std::string name);
+	void disable(std::string name);
+	void pointer(std::string name, uint32_t size, GLenum type);
+	void create();
+	void bind();
+	void unbind();
+	void release();
+	uint32_t getID();
+};
+
+class Program;
+
+// Uniforms
+class Uniforms
+{
+private:
+	Program* prog = nullptr;
+	std::map<std::string, uint32_t> uniforms;
+public:
+	void init(Program* prog);
+	void release();
+
+	void create(std::string name);
+
+	void set1i(std::string name, int x);
+	void set2i(std::string name, int x, int y);
+	void set2i(std::string name, const glm::ivec2& v);
+	void set3i(std::string name, int x, int y, int z);
+	void set3i(std::string name, const glm::ivec3& v);
+	void set4i(std::string name, int x, int y, int z, int w);
+	void set4i(std::string name, const glm::ivec4& v);
+
+	void set1f(std::string name, float x);
+	void set2f(std::string name, float x, float y);
+	void set2f(std::string name, const glm::vec2& v);
+	void set3f(std::string name, float x, float y, float z);
+	void set3f(std::string name, const glm::vec3& v);
+	void set4f(std::string name, float x, float y, float z, float w);
+	void set4f(std::string name, const glm::vec4& v);
+
+	void setMat2(std::string name, const glm::mat2& m);
+	void setMat3(std::string name, const glm::mat3& m);
+	void setMat4(std::string name, const glm::mat4& m);
+};
+
+// Shader
+class Shader
+{
+private:
+	uint32_t id = 0;
+
+	std::string getContent(std::string path);
+	void printShaderLog();
+public:
+
+	void create(GLenum type, std::string path);
+	uint32_t getID();
+	void release();
+};
+
+// Program
+class Program
+{
+private:
+	uint32_t id = 0;
+	std::vector<Shader*> shaders;
+	Attributes attributes;
+	Uniforms uniforms;
+
+	void printProgramLog();
+public:
+	void addShader(Shader* shader);
+	void clearShader();
+
+	void create();
+	void bind();
+	void unbind();
+	void release();
+
+	Attributes* getAttr();
+	Uniforms* getUniforms();
+	uint32_t getID();
+};
+// Texture2D
+// CubeMap
